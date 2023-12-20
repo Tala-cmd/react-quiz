dd31c9d87f76db176b0068a8830440e3eff66706
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-12-20 20:44:50 +0300

    Handling newAnswers case

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,12 +1,21 @@
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
 
-function Options({ question }) {
+
   return (
     <div className="options">
-        {question.options.map((option)=>
-          <button key={option} className="btn btn-option">{option}</button>
-        )}
-      </div>
-  )
+        {question.options.map((option, index) => (
+          <button
+              key={option}
+              className={` btn btn-option ${index === answer && "answer"} 
+              ${hasAnswered ? index === question.correctOption ? "correct" : "wrong" : ""}`}
+              disabled={hasAnswered}
+              onClick={() => dispatch({ type: "newAnswer", payload: index })}>
+              {option}
+          </button>
+        ))}
+    </div>
+  );
 }
 
-export default Options
+export default Options;
