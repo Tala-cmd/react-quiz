bd95d6ef50247652c38a29bfcfa63dccc981f92d
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-12-23 01:52:12 +0300

    Finishing a quiz

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,101 +10,116 @@ import Progress from "./Progress";
 import FinishedScreen from "./FinishedScreen";
 
 const initialState = {
-  questions: [],
+questions: [],
 
-  //loading, error, ready, active, finished
-  status: "loading",
-  index: 0,
-  answer: null,
-  points: 0,
+//loading, error, ready, active, finished
+status: "loading",
+index: 0,
+answer: null,
+points: 0,
+highscore: 0,
 };
 
 function reducer(state, action) {
-  switch (action.type) {
-    case "dataReceived":
-        return {
-          ...state,
-          questions: action.payload,
-          status: "ready",
-        };
-    case "dataFailed":
-        return {
-          ...state,
-          status: "error",
-        };
-    case "start":
-        return {
-          ...state,
-          status: "active",
-        };
+switch (action.type) {
+case "dataReceived":
+    return {
+      ...state,
+      questions: action.payload,
+      status: "ready",
+    };
+case "dataFailed":
+    return {
+      ...state,
+      status: "error",
+    };
+case "start":
+    return {
+      ...state,
+      status: "active",
+    };
 
-    case "newAnswer":
-        // eslint-disable-next-line no-case-declarations
-        const question = state.questions.at(state.index);
-        return {
-          ...state,
-          answer: action.payload,
-          points:
-              action.payload === question.correctOption
-                ? state.points + question.points //points state + points in object
-                : state.points,
-        };
+case "newAnswer":
+    // eslint-disable-next-line no-case-declarations
+    const question = state.questions.at(state.index);
+    return {
+      ...state,
+      answer: action.payload,
+      points:
+          action.payload === question.correctOption
+            ? state.points + question.points //points state + points in object
+            : state.points,
+    };
 
-    case "nextQuestion":
-        return { ...state, index: state.index + 1, answer: null };
+case "nextQuestion":
+    return { ...state, index: state.index + 1, answer: null };
+case 'finish':
+  return{...state, status:'finished', 
+  highscore: state.points > state.highscore ? state.points : state.highscore}
 
-    default:
-        throw new Error("Action unknown");
-  }
+default:
+    throw new Error("Action unknown");
+}
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(
+reducer,
+initialState
+);
 
-  const numQuestions = questions.length;
-  const maxPossiblePoints = questions.reduce(
-    (prev, cur) => prev + cur.points,
-    0
-  );
+const numQuestions = questions.length;
+const maxPossiblePoints = questions.reduce(
+(prev, cur) => prev + cur.points,
+0
+);
 
-  useEffect(function () {
-    fetch("http://localhost:9000/questions")
-        .then((res) => res.json())
-        .then((data) => dispatch({ type: "dataReceived", payload: data }))
-        .catch(() => dispatch({ type: "dataFailed" }));
-  }, []);
+useEffect(function () {
+fetch("http://localhost:9000/questions")
+    .then((res) => res.json())
+    .then((data) => dispatch({ type: "dataReceived", payload: data }))
+    .catch(() => dispatch({ type: "dataFailed" }));
+}, []);
 
-  return (
-    <div className="app">
-        <Header />
-        <Main>
-          {status === "loading" && <Loader />}
-          {status === "error" && <Error />}
-          {status === "ready" && (
-              <StartScreen dispatch={dispatch} numQuestions={numQuestions} />
-          )}
-          {status === "active" && (
-              <>
-                <Progress
-                    index={index}
-                    numQuestions={numQuestions}
-                    points={points}
-                    maxPossiblePoints={maxPossiblePoints}
-                    answer={answer}
-                />
-                <Question
-                    dispatch={dispatch}
-                    question={questions[index]}
-                    answer={answer}
-                />
-                <NextButton dispatch={dispatch} answer={answer} />
-              </>
-          )}
-          {status === 'finished' && <FinishedScreen />}
-        </Main>
-    </div>
-  );
+return (
+<div className="app">
+    <Header />
+    <Main>
+      {status === "loading" && <Loader />}
+      {status === "error" && <Error />}
+      {status === "ready" && (
+          <StartScreen dispatch={dispatch} numQuestions={numQuestions} />
+      )}
+      {status === "active" && (
+          <>
+            <Progress
+                index={index}
+                numQuestions={numQuestions}
+                points={points}
+                maxPossiblePoints={maxPossiblePoints}
+                answer={answer}
+            />
+            <Question
+                dispatch={dispatch}
+                question={questions[index]}
+                answer={answer}
+            />
+            <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+            />
+          </>
+      )}
+      {status === "finished" && (
+          <FinishedScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+          />
+      )}
+    </Main>
+</div>
+);
 }
